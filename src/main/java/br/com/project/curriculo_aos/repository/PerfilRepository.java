f24dd6395b18383ced3b630d7c607496eff48162
Luís <luismsm2003@gmail.com>
2024-06-06 14:58:30 -0300

    fix: changed PerfilInterface name to PerfilRepository

--- src/main/java/br/com/project/curriculo_aos/repository/PerfilRepository.java
+++ src/main/java/br/com/project/curriculo_aos/repository/PerfilRepository.java
@@ -3,5 +3,5 @@ package br.com.project.curriculo_aos.repository;
 import br.com.project.curriculo_aos.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PerfilInterface extends JpaRepository<Perfil, String> {
+public interface PerfilRepository extends JpaRepository<Perfil, String> {
 }
